ae778473cdc26b535c034ce23b238c2b4f9a02e9
Etienne <etienne.tremel@orange.fr>
2012-06-08 09:58:39 +1000

    Now using data to prevent the duplication of the tick box when the init function was called multiple time. Destroy method also added: $('input[type=checkbox]').prettyCheckboxes('destroy');

--- js/prettyCheckboxes.js
+++ js/prettyCheckboxes.js
@@ -9,88 +9,105 @@
 			   long as you leave my infos at the top.
 ------------------------------------------------------------------------- */
 	
-	jQuery.fn.prettyCheckboxes = function(settings) {
-		settings = jQuery.extend({
-					checkboxWidth: 17,
-					checkboxHeight: 17,
-					className : 'prettyCheckbox',
-					display: 'list'
-				}, settings);
-
-		$(this).each(function(){
-			// Find the label
-			$label = $('label[for="'+$(this).attr('id')+'"]');
-
-			// Add the checkbox holder to the label
-			$label.prepend("<span class='holderWrap'><span class='holder'></span></span>");
-
-			// If the checkbox is checked, display it as checked
-			if($(this).is(':checked')) { $label.addClass('checked'); };
-
-			// Assign the class on the label
-			$label.addClass(settings.className).addClass($(this).attr('type')).addClass(settings.display);
-
-			// Assign the dimensions to the checkbox display
-			$label.find('span.holderWrap').width(settings.checkboxWidth).height(settings.checkboxHeight);
-			$label.find('span.holder').width(settings.checkboxWidth);
-
-			// Hide the checkbox
-			$(this).addClass('hiddenCheckbox');
-
-			// Associate the click event
-			$label.bind('click',function(){
-				$('input#' + $(this).attr('for')).triggerHandler('click');
-				
-				if($('input#' + $(this).attr('for')).is(':checkbox')){
-					$(this).toggleClass('checked');
-					$('input#' + $(this).attr('for')).checked = true;
-					
-					$(this).find('span.holder').css('top',0);
-				}else{
-					$toCheck = $('input#' + $(this).attr('for'));
-
-					// Uncheck all radio
-					$('input[name="'+$toCheck.attr('name')+'"]').each(function(){
-						$('label[for="' + $(this).attr('id')+'"]').removeClass('checked');	
-					});
-
-					$(this).addClass('checked');
-					$toCheck.checked = true;
-				};
-			});
-			
-			$('input#' + $label.attr('for')).bind('keypress',function(e){
-				if(e.keyCode == 32){
-					if($.browser.msie){
-						$('label[for="'+$(this).attr('id')+'"]').toggleClass("checked");
-					}else{
-						$(this).trigger('click');
-					}
-					return false;
-				};
-			});
-		});
+(function($){
+	var defaults = {
+		checkboxWidth: 17,
+		checkboxHeight: 17,
+		className : 'prettyCheckbox',
+		display: 'list'
 	};
 	
-	checkAllPrettyCheckboxes = function(caller, container){
-		if($(caller).is(':checked')){
-			// Find the label corresponding to each checkbox and click it
-			$(container).find('input[type=checkbox]:not(:checked)').each(function(){
-				$('label[for="'+$(this).attr('id')+'"]').trigger('click');
-				if($.browser.msie){
-					$(this).attr('checked','checked');
-				}else{
-					$(this).trigger('click');
-				};
+	var methods = {
+		init : function(options) {
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('prettyCheckboxes');
+
+				if(!data) {
+					var settings = $.extend({}, defaults, options),
+						banner = $(this);
+
+					$this.data('prettyCheckboxes', {
+						'id': $this.attr('id')
+					});
+				
+					// Find the label
+					$label = $('label[for="'+$this.attr('id')+'"]');
+		
+					// Add the checkbox holder to the label
+					$label.prepend("<span class='holderWrap'><span class='holder'></span></span>");
+		
+					// If the checkbox is checked, display it as checked
+					if($this.is(':checked')) { $label.addClass('checked'); };
+		
+					// Assign the class on the label
+					$label.addClass(settings.className).addClass($this.attr('type')).addClass(settings.display);
+		
+					// Assign the dimensions to the checkbox display
+					$label.find('span.holderWrap').width(settings.checkboxWidth).height(settings.checkboxHeight);
+					$label.find('span.holder').width(settings.checkboxWidth);
+		
+					// Hide the checkbox
+					$this.addClass('hiddenCheckbox');
+		
+					// Associate the click event
+					$label.bind('click.prettyCheckboxes',function(){
+						$('input#' + $(this).attr('for')).triggerHandler('click');
+						
+						if($('input#' + $(this).attr('for')).is(':checkbox')){
+							$(this).toggleClass('checked');
+							$('input#' + $(this).attr('for')).checked = true;
+							
+							$(this).find('span.holder').css('top',0);
+						}else{
+							$toCheck = $('input#' + $(this).attr('for'));
+		
+							// Uncheck all radio
+							$('input[name="'+$toCheck.attr('name')+'"]').each(function(){
+								$('label[for="' + $(this).attr('id')+'"]').removeClass('checked');	
+							});
+		
+							$(this).addClass('checked');
+							$toCheck.checked = true;
+						};
+					});
+					
+					$('input#' + $label.attr('for')).bind('keypress.prettyCheckboxes',function(e){
+						if(e.keyCode == 32){
+							if($.browser.msie){
+								$('label[for="'+$(this).attr('id')+'"]').toggleClass("checked");
+							}else{
+								$(this).trigger('click');
+							}
+							return false;
+						};
+					});
+				}
 			});
-		}else{
-			$(container).find('input[type=checkbox]:checked').each(function(){
-				$('label[for="'+$(this).attr('id')+'"]').trigger('click');
-				if($.browser.msie){
-					$(this).attr('checked','');
-				}else{
-					$(this).trigger('click');
-				};
+		},
+		destroy : function( ) {		
+			return this.each(function() {
+				var $this = $(this);
+				
+				$(window).unbind('.prettyCheckboxes');
+				
+				$('label[for="'+$this.data('prettyCheckboxes').id +'"] .holderWrap').remove();
+				$('label[for="'+$this.data('prettyCheckboxes').id +'"]').removeClass('prettyCheckbox checked checkbox radio list');
+				$this.removeData('prettyCheckboxes');		
+				$this.removeClass('hiddenCheckbox');
+				
 			});
-		};
+		}
 	};
+	
+	$.fn.prettyCheckboxes = function( method ) {
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.prettyCheckboxes' );
+		}    
+	};
+
+})(jQuery);
